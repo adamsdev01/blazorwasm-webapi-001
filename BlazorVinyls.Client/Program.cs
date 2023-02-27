1ae34328ea23ff214851bcbcab49dcd10c0a5e09
Adams-Developer <tadams03@mail.roosevelt.edu>
2023-02-26 18:26:19 -0600

    Fix wasm app

--- BlazorVinyls.Client/Program.cs
+++ BlazorVinyls.Client/Program.cs
@@ -2,16 +2,16 @@ using BlazorVinyls.Client;
 using BlazorVinyls.Client.HttpRepository;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.FileProviders;
 using Tewr.Blazor.FileReader;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7092/api/") });
 builder.Services.AddScoped<IVinylHttpRepository, VinylHttpRepository>();
 builder.Services.AddFileReaderService(o => o.UseWasmSharedBuffer = true);
 
 await builder.Build().RunAsync();
+
